4f15db02deb0b10440d2f91fee10f1563e21fbaf
John Deeney <JohnCDeeney@gamil.com>
2015-04-04 23:05:00 -0400

    Created basic framework with CSSs.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
+		<link rel="shortcut icon" href="img/selfie.png">
+	</head>
+	
+	<div class="transbox">
+		Text
+	</div>
+</div>
+</html>
---
+++ projects.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/projects.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
+	</head>
+	
+	<div class="transbox">
+		<h1>
+			These are my projects...
+		</h1>
+	</div>
+</div>
+</html>
