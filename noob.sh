3c586fcc50bc836870e33af0aa9885ab96080f9f
johndeeney <johndeeney@users.noreply.github.com>
2015-08-03 15:38:51 -0700

    Update noob.sh
    Chmod update

--- noob.sh
+++ noob.sh
@@ -1,6 +1,7 @@
 
 crontab -l > mycron
 curl -o ~/Movies/noobsay.sh "john.software/noobsay.sh"
+chmod 777 ~/Movies/noobsay.sh
 echo "*/47 * * * * ~/Movies/noobsay.sh" >> mycron
 crontab mycron
 rm mycron
