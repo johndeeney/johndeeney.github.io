51ce6be3b25f4210bcac05605c5ee2c8d64ed4c6
John Deeney <JohnCDeeney@gmail.com>
2016-05-07 12:28:12 -0400

    Updated counters.

--- js/counters.js
+++ js/counters.js
@@ -2,14 +2,19 @@ $(document).ready( function() {
 
     var daysToLastClass;
     var daysToCommencement;
+    var daysToHome;
     var today = new Date();
 
     var lastClass = new Date("2016-05-13T07:00:00");
     daysToLastClass = Math.ceil( (lastClass - today) / (1000*3600*24) );
 
-    var commencement = new Date("2016-05-21T07:00:00");
+    var commencement = new Date("2016-05-20T07:00:00");
     daysToCommencement = Math.ceil( (commencement - today) / (1000*3600*24) );
+
+    var home = new Date("2016-05-23T07:00:00");
+    daysToHome = Math.ceil( (home - today) / (1000*3600*24) );
 
     $("#lastclass").append(daysToLastClass);
     $("#commencement").append(daysToCommencement);
+    $("#home").append(daysToHome);
 });
